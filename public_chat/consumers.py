d36c3b74c64c24702c5356340744e0f766e7674a
Andi Fathul <35810127+andifathulms@users.noreply.github.com>
2021-09-23 11:30:23 +0700

    handling consumer error, msg format, timestamp

--- public_chat/consumers.py
+++ public_chat/consumers.py
@@ -1,8 +1,17 @@
+from django.contrib.auth import get_user_model
+from django.contrib.humanize.templatetags.humanize import naturaltime, naturalday
+from django.utils import timezone
+
 from channels.generic.websocket import AsyncJsonWebsocketConsumer
 import json
-from django.contrib.auth import get_user_model
+
+from datetime import datetime
+
+
 
 User = get_user_model()
+
+MSG_TYPE_MESSAGE = 0  # For standard messages
 
 # Example taken from:
 # https://github.com/andrewgodwin/channels-examples/blob/master/multichat/chat/consumers.py
@@ -41,10 +50,19 @@ class PublicChatConsumer(AsyncJsonWebsocketConsumer):
 		command = content.get("command", None)
 		print("PublicChatConsumer: receive_json: " + str(command))
 		print("PublicChatConsumer: receive_json: message: " + str(content["message"]))
-		if command == "send":
-			if len(content["message"].lstrip()) == 0:
-				raise Exception("You can't send an empty message.")
-			await self.send_message(content["message"])
+		
+		try:
+			if command == "send":
+				if len(content["message"].lstrip()) == 0:
+					raise ClientError(422,"You can't send an empty message.")
+				await self.send_message(content["message"])
+		except ClientError as e:
+			# Catch any errors and send it back
+			errorData = {}
+			errorData['error'] = e.code
+			if e.message:
+				errorData['message'] = e.message
+			await self.send_json(errorData)
 
 
 	async def send_message(self,message):
@@ -65,19 +83,49 @@ class PublicChatConsumer(AsyncJsonWebsocketConsumer):
 		"""
 		# Send a message down to the client
 		print("PublicChatConsumer: chat_message from user #" + str(event["user_id"]))
+		timestamp = calculate_timestamp(timezone.now())
 		await self.send_json(
 			{
+				"msg_type": MSG_TYPE_MESSAGE,
 				"profile_image": event["profile_image"],
 				"username": event["username"],
 				"user_id": event["user_id"],
 				"message": event["message"],
+				"natural_timestamp": timestamp,
 			},
 		)
 
+class ClientError(Exception):
+    """
+    Custom exception class that is caught by the websocket receive()
+    handler and translated into a send back to the client.
+    """
+    def __init__(self, code, message):
+        super().__init__(code)
+        self.code = code
+        if message:
+        	self.message = message
 
-
-
-
+def calculate_timestamp(timestamp):
+    """
+    1. Today or yesterday:
+        - EX: 'today at 10:56 AM'
+        - EX: 'yesterday at 5:19 PM'
+    2. other:
+        - EX: 05/06/2020
+        - EX: 12/28/2020
+    """
+    ts = ""
+    # Today or yesterday
+    if (naturalday(timestamp) == "today") or (naturalday(timestamp) == "yesterday"):
+        str_time = datetime.strftime(timestamp, "%I:%M %p")
+        str_time = str_time.strip("0")
+        ts = f"{naturalday(timestamp)} at {str_time}"
+    # other days
+    else:
+        str_time = datetime.strftime(timestamp, "%m/%d/%Y")
+        ts = f"{str_time}"
+    return str(ts)
 
 
 
