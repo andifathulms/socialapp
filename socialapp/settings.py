a71c0f1d3ce6db0d27621331b36fcbd6e99e5d32
Andi Fathul <35810127+andifathulms@users.noreply.github.com>
2021-10-09 14:55:25 +0700

    add git ignore and aws s3 environ

--- socialapp/settings.py
+++ socialapp/settings.py
@@ -13,6 +13,12 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 
+import environ
+
+#initialise environ
+env = environ.Env()
+environ.Env.read_env()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -21,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4^52@bae=edb46jzqm2-h#%9_wh5acblz=x#v=ho(98k)8_ero'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -42,6 +48,7 @@ INSTALLED_APPS = [
 
     'channels',
     'crispy_forms',
+    'storages',
 
     'personal',
     'account',
@@ -186,3 +193,12 @@ DATE_INPUT_FORMATS = [
 ]
 
 LOGIN_URL = '/login'
+
+AWS_ACCESS_KEY_ID=env('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY=env('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME=env('AWS_STORAGE_BUCKET_NAME')
+
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None
+
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
