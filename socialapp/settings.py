df3ac611ad1395928e3e74e779c6598bb6d6f3ee
Andi Fathul <35810127+andifathulms@users.noreply.github.com>
2021-10-10 22:11:02 +0700

    add django-heroku2

--- socialapp/settings.py
+++ socialapp/settings.py
@@ -14,7 +14,6 @@ from pathlib import Path
 import os
 import django_heroku
 
-django_heroku.settings(locals())
 """
 import environ
 
@@ -221,3 +220,4 @@ AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+django_heroku.settings(locals())
