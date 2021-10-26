744c04157a827f3976c365fb6bb487a5aad51b54
Andi Fathul <35810127+andifathulms@users.noreply.github.com>
2021-10-27 02:27:34 +0700

    marketplace CREATE

--- marketplace/urls.py
+++ marketplace/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from .views import(
 	MarketplaceListView,
 	MarketplaceDetailView,
+	MarketplaceCreateView,
+	load_subcategories,
 	)
 
 app_name = 'marketplace'
@@ -10,4 +12,6 @@ app_name = 'marketplace'
 urlpatterns = [
 	path('', MarketplaceListView.as_view(), name='marketplace-home'),
 	path('<int:pk>/', MarketplaceDetailView.as_view(), name='marketplace-detail'),
+	path('create/', MarketplaceCreateView.as_view(), name='marketplace-create'),
+	path('ajax/load-cat/', load_subcategories, name='ajax_load_subcat'),
 ]
--- marketplace/views.py
+++ marketplace/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views import View
+from django.views.decorators.csrf import csrf_exempt
 
-from .models import Product, ProductCategory, ProductCondition
+from .models import Product, ProductImage, ProductCategory, ProductSubCategory, ProductCondition, ProductAvailability
+from .forms import ProductForm
 
 class MarketplaceListView(LoginRequiredMixin, View):
 	login_url = '/login/'
@@ -22,13 +24,74 @@ class MarketplaceListView(LoginRequiredMixin, View):
 
 		return render(request, 'marketplace/marketplace.html', context)
 
-class MarketplaceDetailView(LoginRequiredMixin, View):
-    def get(self, request, pk, *args, **kwargs):
-        product = Product.objects.get(pk=pk)
-        
-        context = {
-            'product': product,
-        }
+	def post(self, request, *args, **kwargs):
+		print("list")
 
-        return render(request, 'marketplace/marketplace_detail.html', context)
+class MarketplaceDetailView(LoginRequiredMixin, View):
+	login_url = '/login/'
+	redirect_field_name = 'redirect_to'
+
+	def get(self, request, pk, *args, **kwargs):
+		product = Product.objects.get(pk=pk)
+
+		context = {
+			'product': product,
+		}
+
+		return render(request, 'marketplace/marketplace_detail.html', context)
+
+	def post(self, request, *args, **kwargs):
+		print("det")
+
+class MarketplaceCreateView(LoginRequiredMixin, View):
+	login_url = '/login/'
+	redirect_field_name = 'redirect_to'
+
+	def get(self, request, *args, **kwargs):
+		condition = ProductCondition.objects.all()
+		availability = ProductAvailability.objects.all()
+		categories = ProductCategory.objects.all()
+
+		context = {}
+		context["conditions"] = condition
+		context["availabilities"] = availability
+		context["categories"] = categories
+
+		return render(request, 'marketplace/marketplace_create.html', context)
+
+	def post(self, request, *args, **kwargs):
+		form = ProductForm(request.POST, request.FILES)
+		files = request.FILES.getlist('image')
+		
+		if form.is_valid():
+			new_post = form.save(commit=False)
+			new_post.author = request.user
+			new_post.save()
+
+			for f in files:
+				img = ProductImage(image=f)
+				img.save()
+				new_post.image.add(img)
+		
+		else:
+			print(form.errors)
+
+		context = {}
+
+		product = Product.objects.all()
+		category = ProductCategory.objects.all()
+		condition = ProductCondition.objects.all()
+
+		context["products"] = product
+		context["categories"] = category
+		context["conditions"] = condition
+
+		return render(request, 'marketplace/marketplace.html', context)
+
+@csrf_exempt
+def load_subcategories(request):
+    category = request.GET.get('category')
+    subcategory = ProductSubCategory.objects.filter(category=category).order_by('name')
+    print(subcategory)
+    return render(request, 'marketplace/snippets/subcategory.html', {'subcategories': subcategory})
 
