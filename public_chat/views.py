bb8a5452f6efa56c314292e501a5a29d98f42b61
Andi Fathul <35810127+andifathulms@users.noreply.github.com>
2022-01-04 11:23:00 +0700

    prod settings

--- public_chat/views.py
+++ public_chat/views.py
@@ -8,7 +8,7 @@ DEBUG = False
 def public_chat_view(request):
 	context = {}
 	# context['debug_mode'] = settings.DEBUG
-	context['debug_mode'] = False
+	context['debug_mode'] = settings.DEBUG
 	context['debug'] = DEBUG
 	context['room_id'] = "1"
 	fillRightNav(request,context)
